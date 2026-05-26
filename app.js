5bf7c0789aff764c531db39be0eb827805259b73
Charles Fontaine <fontaine.charles64@gmail.com>
2026-05-26 20:50:52 +0200

    fix(app): reorder dotenv config to call the env variables first and then the connection to the bdd + reorder call cors

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
+require('dotenv').config();
+require('./models/connection');
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-require('dotenv').config();
-require('./connection');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-const cors = require('cors');
-
 var app = express();
+
+const cors = require('cors');
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,7 +20,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
