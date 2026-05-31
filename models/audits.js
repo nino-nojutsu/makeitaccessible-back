c381d7f9317eddc300b7a5f4f37455b0537c43e4
Charles Fontaine <fontaine.charles64@gmail.com>
2026-05-31 20:51:36 +0200

    feat(audit): delete users ref. doc for the audits model because it is saved by the site collection : a user can have multiple sites so sites docs refer to a user id doc

--- models/audits.js
+++ models/audits.js
@@ -13,7 +13,6 @@ const auditSchema = mongoose.Schema({
     incomplete:   Number,  // nombre total de critères non testables
     score:        Number,  // pourcentage (passed / (passed + failed)) * 100
   },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'users', default: null }, // réf. vers la collection users ou null si audit anonyme
   site: { type: mongoose.Schema.Types.ObjectId, ref: 'sites' }, // réf. vers la collection sites
 });
 
