771ae99e46a4f157e16ebc0f2fcda9a78173c4be
Charles Fontaine <charles@zoomalia.com>
2026-06-02 16:05:19 +0200

    feat(models): add details about data type and default data for the audit schema

--- models/audits.js
+++ models/audits.js
@@ -1,21 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const auditSchema = mongoose.Schema({
-  url: String,
-  status: String,
-  errorMessage: String,
-  createdAt: Date,
+  url: { type: String, default: null }, // url du site à auditer
+  status: { type: String, default: "pending" }, // status d'état de l'analyse renvoyé par axe-core
+  errorMessage: { type: String, default: null }, // message d'erreur si l'audit à échouer renvoyé par axe-core
+  createdAt: { type: Date, default: Date.now() }, // date de création de l'audit
   summary: {
-    total:        Number,  // nombre total de critères évalués
-    passed:       Number,  // nombre total de critères validés
-    failed:       Number,  // nombre total de critères non valides
-    inapplicable: Number,  // nombre total de critères non applicables
-    incomplete:   Number,  // nombre total de critères non testables
-    score:        Number,  // pourcentage (passed / (passed + failed)) * 100
+    total: { type: Number, default: 0 }, // nombre total de critères évalués
+    passed: { type: Number, default: 0 }, // nombre total de critères validés
+    failed: { type: Number, default: 0 }, // nombre total de critères non valides
+    inapplicable: { type: Number, default: 0 }, // nombre total de critères non applicables
+    incomplete: { type: Number, default: 0 }, // nombre total de critères non testables
+    score: { type: Number, default: 0 }, // pourcentage (passed / (passed + failed)) * 100
   },
-  site: { type: mongoose.Schema.Types.ObjectId, ref: 'sites' }, // réf. vers la collection sites
+  site: { type: mongoose.Schema.Types.ObjectId, ref: "sites" }, // réf. vers la collection sites
 });
 
-const Audit = mongoose.model('audits', auditSchema);
+const Audit = mongoose.model("audits", auditSchema);
 
 module.exports = Audit;
