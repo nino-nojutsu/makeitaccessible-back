7272f3e7cd910d058092538667ca21a1b5a22eba
Charles Fontaine <fontaine.charles64@gmail.com>
2026-06-03 18:57:08 +0200

    feat(audits): rename and clean key of the audit schema

--- models/audits.js
+++ models/audits.js
@@ -3,15 +3,14 @@ const mongoose = require("mongoose");
 const auditSchema = mongoose.Schema({
   url: { type: String, default: null }, // url du site à auditer
   status: { type: String, default: "pending" }, // status d'état de l'analyse renvoyé par axe-core
-  errorMessage: { type: String, default: null }, // message d'erreur si l'audit à échouer renvoyé par axe-core
   createdAt: { type: Date, default: Date.now() }, // date de création de l'audit
   summary: {
+    inapplicable: { type: Number, default: 0 }, // nombre total de critères innaplicable
+    passes: { type: Number, default: 0 }, // nombre total de critères validés
+    incomplete: { type: Number, default: 0 }, // nombre total de critères incomplet
+    violations: { type: Number, default: 0 }, // nombre total de critères en anomalie
     total: { type: Number, default: 0 }, // nombre total de critères évalués
-    passed: { type: Number, default: 0 }, // nombre total de critères validés
-    failed: { type: Number, default: 0 }, // nombre total de critères non valides
-    inapplicable: { type: Number, default: 0 }, // nombre total de critères non applicables
-    incomplete: { type: Number, default: 0 }, // nombre total de critères non testables
-    score: { type: Number, default: 0 }, // pourcentage (passed / (passed + failed)) * 100
+    score: { type: Number, default: 0 }, // pourcentage de réussite : (passes / (passes + incomplete + violations) * 100)
   },
   site: { type: mongoose.Schema.Types.ObjectId, ref: "sites" }, // réf. vers la collection sites
 });
